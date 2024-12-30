33c3ab11fdd982dc6cec3d240cc4252634519f23
Rahul Tak <rahultak955@gmail.com>
2024-12-30 13:44:18 +0530

    login and signup process completed

--- project-02_blog/index.js
+++ project-02_blog/index.js
@@ -1,14 +1,21 @@
 const path = require("path"); // 1. views folder ko use karne ke liye path ka use karte hai 
 const express = require('express');
+const userRoute = require('./routes/user')
+const mongoose = require('mongoose')
 
 const app= express(); // 2. express js se ak app banaya jo 8000 port pe chalega 
 const PORT = 8000;
+mongoose.connect('mongodb://127.0.0.1:27017/blogify').then((e)=> console.log("Mongodb connectd"))
 
 app.set("view engine","ejs"); // 3. nodejs ko bataya ki html ko show kar ne ke liye ejs ka use kar rahe hai 
-app.set("view",path.resolve("./views")); // 4.html ke pages kaha hai vo bataya 
+app.set("views", path.resolve("./views")); // 4.html ke pages kaha hai vo bataya 
 
-app.get('/',(req,res)=>{
+app.use(express.urlencoded({ extended: false})) // post se jo date send karte hai to body me json format me add karne ke liye 
+
+app.get("/",(req,res)=>{
     res.render('home')
 })
+app.use("/user",userRoute);
 
-app.listen(PORT,()=> console.log(`server started at PORT:${PORT}`)) // 5. app ko loaclhost pe host karne ke liye 
+
+app.listen(PORT,()=> console.log(`server started at PORT: ${PORT}`)) // 5. app ko loaclhost pe host karne ke liye 
---
+++ project-02_blog/models/user.js
@@ -0,0 +1,69 @@
+const {createHmac , randomBytes} = require("crypto"); // 8. password jo has pe convert karne ke liye 
+const {Schema, model}= require("mongoose"); // 6. user create kar ne ke liye phle schema banana padta hai 
+
+// schema create kiya 
+const userSchema = new Schema({
+    fullName:{
+        type: String,
+        required: true,
+    },
+    email:{
+        type:String,
+        required:true,
+        unique: true, // user same email se 2 dar sign na kar paye 
+    },
+    salt:{
+        type:String,
+        
+    },
+    password:{
+        type:String,
+        required:true,
+    },
+    profileImageURL:{
+        type:String,
+        default: "/images/default.svg",
+    },
+    role:{
+        type:String,
+        enum:["USER","ADMIN"],
+        default:"USER"
+    }
+},{timestamps: true})
+
+//  9. pre matlab user ko save kar ne se phle pre function call karna 
+userSchema.pre("save", function (next) {
+    const user = this;
+
+    if(!user.isModified("password")) return;
+
+    const salt = randomBytes(16).toString();
+    const hashedPassword = createHmac("sha256",salt)
+        .update(user.password)
+        .digest("hex")
+    
+    this.salt = salt;
+    this.password = hashedPassword;
+
+    next();
+})
+
+// ak function banaya jisme password match karenge 
+userSchema.static("matchPassword",async function(email,password){
+    const user =await this.findOne({email});
+    if(!user ) throw new Error('User not found');
+    // user ka password ko db me se lene ke liye 
+    const salt = user.salt;
+    const hashedPassword = user.password;
+    // user login kar ne aaya to uske pass ko has me convert kar ne ke baad check kar ne liye 
+    const userProvidedHash = createHmac("sha256",salt) // jo user ne password dala hai usko salt me change kiya or check kiya 
+    .update(password)
+    .digest("hex")
+
+    if(hashedPassword !== userProvidedHash) throw new Error('password do not match')
+    return user;
+})
+
+const User = model('user',userSchema); // 7. schema crete hone ke baad model crete hota hai 
+
+module.exports = User;
