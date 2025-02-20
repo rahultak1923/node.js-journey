6c298b2e654c19aa65f42143e23c7340381adce6
Rahul Tak <rahultak955@gmail.com>
2025-02-20 12:02:59 +0530

    curd op

---
+++ crud-op/models/user.js
@@ -0,0 +1,23 @@
+const { Schema, model } = require("mongoose");
+
+const userSchema = new Schema({
+    fullName:{
+        type:String,
+        required:true,
+    },
+    email:{
+        type:String,
+        required:true,
+    },
+    // salt:{
+    //     type:String,
+    // },
+    password:{
+        type:String,
+        required:true,
+    }
+},{timestamps:true})
+
+const User = model('user',userSchema);
+
+module.exports = User;
